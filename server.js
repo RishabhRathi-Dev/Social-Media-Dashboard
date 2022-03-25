b5f624736662effa5288a1799cdf8bf80b41f028
Rishabh Rathi <72991781+RishabhRathi-Dev@users.noreply.github.com>
2022-03-25 22:26:37 +0530

    base youtube api added
    meta data like subs count, total view count and video count added.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var http = require("http");
 var url = require('url');
+const { google } = require("googleapis")
+require('dotenv').config();
 
 var express = require("express");
 const res = require("express/lib/response");
+const { ids } = require("googleapis/build/src/apis/ids");
+const { json } = require("express/lib/response");
 
 var app = express()
 
@@ -19,6 +23,13 @@ app.get('/index.html', function(req, res){
 })
 
 // Use Routers for all the connections 
+
+function jsonParser(stringValue) {
+
+    var string = JSON.stringify(stringValue);
+    var objectValue = JSON.parse(string);
+    return objectValue;
+}
 
 // Sign in and log
 
@@ -37,3 +48,27 @@ app.get('/views/selectplatform.html', function(req, res){
 })
 
 // API
+
+
+// This part gets viewcount, subscriber count and video count of the entered youtube channel
+google.youtube("v3").channels.list({
+    key: process.env.YOUTUBE_API,
+    part: 'statistics',
+    forUsername: 'Pewdiepie',
+}).then((response) =>{
+    console.log("statistics")
+    const {data} = response;
+    //console.log(data)
+    const {items} = data;
+
+    items.forEach(element => {
+        //console.log(element)
+        const {statistics} = element
+        var StatArray = Object.values(statistics)
+        //console.log(statistics)
+
+        var viewCount = StatArray[0]
+        var subscriberCount = StatArray[1]
+        var videoCount = StatArray[3]
+    });
+}).catch((err)=> console.log(err))
