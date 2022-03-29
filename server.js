1abcf2bb710d500ed9668adc684144cc7d6b3ccc
Rishabh Rathi <72991781+RishabhRathi-Dev@users.noreply.github.com>
2022-03-30 02:00:13 +0530

    twitter api up and running fine

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const { json } = require("express/lib/response");
 
 var app = express()
 
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
 const server = app.listen(0 || 8082, ()=>{
     console.log('Listening on port:', server.address().port);
 });
@@ -45,17 +49,21 @@ app.get('/views/selectplatform.html', function(req, res){
 
 // Youtube
 
+//RetriveData()
+
+async function YoutubeAPIcall(){
+
 var YoutubeData = []
 
 // This part gets viewcount, subscriber count and video count of the entered youtube username
 google.youtube("v3").channels.list({
     key: process.env.YOUTUBE_API,
     part: 'statistics, contentDetails', // 'snippet'
-    forUsername: "Ask Gamedev", // change to a varaiable // point to be noted username is not always same as channel name
+    forUsername: "Pewdiepie", // change to a varaiable // point to be noted username is not always same as channel name
 }).then((response) =>{
     //console.log("statistics")
     const {data} = response;
-    console.log(data)
+    //console.log(data)
     const {items} = data;
 
     items.forEach(element => {
@@ -77,7 +85,7 @@ google.youtube("v3").channels.list({
         var subscriberCount = StatArray[1]
         var videoCount = StatArray[3]
 
-        YoutubeData.push(statsArray)
+        EnterDatainYData1(StatArray)
     });
 }).catch((err)=> console.log(err))
 
@@ -93,13 +101,13 @@ function getUploadedPlaylistData(channelId){
         part: 'snippet',
         channelId: channelId,
         type: "video",
-        maxResults: "50",
+        maxResults: "20",
         order: 'date',
         safeSearch: 'none',
         videoType: 'any',
         videoDuration: 'any',
     }).then((response) => {
-        console.log(response) // modify response to get data 
+        //console.log(response) // modify response to get data 
         var resData = response.data
         const {items} = resData
 
@@ -111,11 +119,11 @@ function getUploadedPlaylistData(channelId){
             //console.log(element)
 
             var elementDataArray = Object.values(element)
-            console.log(elementDataArray)
+            //console.log(elementDataArray)
 
             var videokindobject = elementDataArray[2]
             var videokindarray = Object.values(videokindobject)
-            console.log(videokindarray)
+            //console.log(videokindarray)
             var videoID = videokindarray[1]
             //console.log(videoID)
             
@@ -153,11 +161,15 @@ function getVideoData(videoID, videoTitle, key, date){
             //console.log(statsArray)
 
             statsArray.push(videoTitle) // 4 : videoTitle
-            statsArray.push(key)
-            VideoMetricsArray[date] = statsArray
+
+            EnterDatainYData(date, statsArray)
         })
 
         
+    }).then((res) => {
+        FinaliseYData()
+    }).catch((err) => {
+        console.log(err)
     }).catch((err)=> {
         console.log(err)
     })
@@ -167,63 +179,97 @@ function getVideoData(videoID, videoTitle, key, date){
 //console.log(VideoMetricsArray)
 YoutubeData.push(VideoMetricsArray)
 
+async function EnterDatainYData1(statsArray){
+    YoutubeData.push(statsArray)
+}
+
+async function EnterDatainYData(date, statsArray){
+    VideoMetricsArray[date] = statsArray
+}
+
+function FinaliseYData(){
+    if (!VideoMetricsArray in YoutubeData){
+        YoutubeData.push(VideoMetricsArray)
+        //console.log(YoutubeData)
+    }
+}
+
+
+return YoutubeData;
+
+}
+
+async function RetriveData(){
+    var finalYData = await YoutubeAPIcall()
+    finalYData = JSON.stringify(finalYData)
+    console.info(finalYData)
+}
 
 // Twitter (Completed Needed things) (Can get a array of redirecting link to the specified tweet or we can emmbed it)
 
-var twitterData = [] // contains everydata from twitter needed // 0 index is user data entry after that there is data of 100 latest tweets 
 
-// need to set up auth anyways
-const {TwitterApi} = require('twitter-api-v2');
-const { count } = require("console");
+app.post("/twitterdata", function(req, res){
+    var twitterData; // contains everydata from twitter needed // 0 index is user data entry after that there is data of 100 latest tweets 
 
-const client = new TwitterApi({
-    appKey: process.env.TWITTER_API_KEY,
-    appSecret: process.env.TWITTER_API_SECRET,
-    accessToken: process.env.TWITTER_ACCESS_TOKEN,
-    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-});
+    var TUSERNAME = req.body.Twitter
+    // need to set up auth anyways
+    const {TwitterApi} = require('twitter-api-v2');
+    const { count } = require("console");
 
-// thinking of keeping the tweets info for latest 20 tweets only for now
+    const client = new TwitterApi({
+        appKey: process.env.TWITTER_API_KEY,
+        appSecret: process.env.TWITTER_API_SECRET,
+        accessToken: process.env.TWITTER_ACCESS_TOKEN,
+        accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    });
 
-// change to a variable
-client.v2.userByUsername('akshaykumar', {"user.fields": "public_metrics"}).then((val) => {
-    //console.log(val)
-    var dataArray = val.data
-    var userID = dataArray.id
-    //console.log(userID)
+    // thinking of keeping the tweets info for latest 20 tweets only for now
 
-    var userMetricsObject = dataArray.public_metrics // This will have total followers_count, following_count, listed_count, tweet_count
-    var userMetricsArray = Object.values(userMetricsObject)
+    // change to a variable
+    client.v2.userByUsername('akshaykumar', {"user.fields": "public_metrics"}).then((val) => {
+        //console.log(val)
+        twitterData = []
+        var dataArray = val.data
+        var userID = dataArray.id
+        //console.log(userID)
 
-    twitterData.push(userMetricsArray)
+        var userMetricsObject = dataArray.public_metrics // This will have total followers_count, following_count, listed_count, tweet_count
+        var userMetricsArray = Object.values(userMetricsObject)
 
-    //console.log(userMetricsArray)
+        twitterData.push(userMetricsArray)
 
-    
-    client.v2.userTimeline(userID, {"tweet.fields": "public_metrics", "max_results": "100"}).then((val2) => {
-        //console.log(val2)
-        var dataArray2 = val2.data
-        var last100tweetArray = dataArray2.data
+        //console.log(userMetricsArray)
+
         
-        last100tweetArray.forEach(element => {
-            //console.log(element)
-            var metricsObjects = element.public_metrics  // last 100 tweets data required data from it :: retweet_count, reply_count, like_count, quote_count
-            var tweetText = element.text
-            var metricsArray = Object.values(metricsObjects)
-            metricsArray.push(tweetText)
-            //console.log(metricsObjects)
-            //console.log(metricsArray)
-            twitterData.push(metricsArray)
+        client.v2.userTimeline(userID, {"tweet.fields": "public_metrics", "max_results": "20"}).then((val2) => {
+            //console.log(val2)
+            var dataArray2 = val2.data
+            var last100tweetArray = dataArray2.data
+            
+            last100tweetArray.forEach(element => {
+                //console.log(element)
+                var metricsObjects = element.public_metrics  // last 100 tweets data required data from it :: retweet_count, reply_count, like_count, quote_count
+                var tweetText = element.text
+                var metricsArray = Object.values(metricsObjects)
+                metricsArray.push(tweetText)
+                //console.log(metricsObjects)
+                //console.log(metricsArray)
+                twitterData.push(metricsArray)
+            })
+            
+        }).then((res) =>{
+            //console.log(twitterData)
+            return twitterData
+        }).then((response) => {
+            res.send(JSON.stringify(response))
+        }).catch((err) => {
+            console.log(err)
         })
-        
+    
     }).catch((err) => {
         console.log(err)
     })
 
-}).catch((err) => {
-    console.log(err)
-})
-
-
-
+}
+)
 
